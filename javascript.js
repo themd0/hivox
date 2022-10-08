272904a21522b5976e86e5e9528f19f4fe136ddb
themd0 <90915458+themd0@users.noreply.github.com>
2022-10-08 01:55:59 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -20,11 +20,4 @@ $('.tag-manga').each(function() {
 });
 
 
-$('.nsv-5').each(function() {
-  $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?package=com.anifort.player'));
-});
 
-
-$('.tag-harem, .tag-action, .tag-shounen, .tag-sports, .tag-games, .tag-micha, .tag-ecchi, .tag-shoujou, .tag-sci-fi, .tag-drama, .tag-demons, .tag-army, .tag-fantasy, .tag-comedy, .tag-mystery, .tag-vampire, .tag-unn, .tag-history, .tag-nlife, .tag-seinen, .tag-romance, .tag-romance, .tag-super, .tag-powers, .tag-horror, .tag-psycho, .tag-school, .tag-music, .tag-thriller, .tag-crime, .tag-shoujo, .tag-police, .tag-jussi, .tag-thriller, .tag-isekai, .tag-magic, .tag-fights, .tag-nlife, .tag-adventure, .tag-VG, .tag-animeism, .tag-BR, .tag-HSG, .tag-manga, .tag-short, .tag-chinese, .tag-novel, .xx-button, .xx-button1, .xx-year, .xx-age, .xx-rate').each(function() {
-  $(this).attr('href', $(this).attr('href').replace('/','#'));
-});
