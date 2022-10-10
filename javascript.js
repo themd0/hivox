41d71ec86010ed7bb58af8846ee13cc50f613af7
themd0 <90915458+themd0@users.noreply.github.com>
2022-10-10 22:54:20 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -21,5 +21,7 @@ $('.nsv-5').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?package=com.anifort.player'));
 });
 
-
+$('.nsv-4').each(function() {
+  $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?package=com.dv.adm'));
+});
 
