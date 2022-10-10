6b990835bca2d3dbcbb4d50cc3b15c5c6ab5258d
themd0 <90915458+themd0@users.noreply.github.com>
2022-10-10 13:55:55 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -15,9 +15,23 @@ function myFunction() {
     }
 }
 
-$('.tag-harem, .tag-action, .tag-shounen, .tag-sports, .tag-games, .tag-micha, .tag-ecchi, .tag-shoujou, .tag-sci-fi, .tag-drama, .tag-demons, .tag-army, .tag-fantasy, .tag-comedy, .tag-mystery, .tag-vampire, .tag-unn, .tag-history, .tag-nlife, .tag-seinen, .tag-romance, .tag-romance, .tag-super, .tag-powers, .tag-horror, .tag-psycho, .tag-school, .tag-music, .tag-thriller, .tag-crime, .tag-shoujo, .tag-police, .tag-jussi, .tag-thriller, .tag-isekai, .tag-magic, .tag-fights, .tag-nlife, .tag-adventure, .tag-VG, .tag-animeism, .tag-BR, .tag-HSG, .tag-manga, .tag-short, .tag-chinese, .tag-novel, .xx-button, .xx-button1, .xx-yearx, .xx-age, .xx-rate').each(function() {
-  $(this).attr('href', $(this).attr('href').replace('/p/','#'));
-});
+function toggleDiv(divid)
+  {
+    varon = divid + 'on';
+    varoff = divid + 'off';
+ 
+    if(document.getElementById(varon).style.display == 'block')
+    {
+    document.getElementById(varon).style.display = 'none';
+    document.getElementById(varoff).style.display = 'block';
+    }
+   
+    else
+    {  
+    document.getElementById(varoff).style.display = 'none';
+    document.getElementById(varon).style.display = 'block'
+    }
+} 
 
 $('.nsv-3').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?package=com.anifort.player'));
