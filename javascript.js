71efc775c7de24580300dbe6d1393c09e595e3e0
themd0 <90915458+themd0@users.noreply.github.com>
2022-10-14 20:35:44 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,22 +1,3 @@
-
-function toggleDiv(divid)
-  {
-    varon = divid + 'on';
-    varoff = divid + 'off';
- 
-    if(document.getElementById(varon).style.display == 'block')
-    {
-    document.getElementById(varon).style.display = 'none';
-    document.getElementById(varoff).style.display = 'block';
-    }
-   
-    else
-    {  
-    document.getElementById(varoff).style.display = 'none';
-    document.getElementById(varon).style.display = 'block'
-    }
-} 
-
 $('.nsv-2').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?target=video'));
 });
@@ -27,9 +8,4 @@ $('.nsv-4, .d-ak-360, .d-ak-720').each(function() {
 
 $('.nsv-5, .ak-but-h, .ak-but-s, .w-ak-360, .w-ak-720').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?package=com.anifort.player'));
-});
-
-Array.from(document.querySelectorAll('img')).forEach(x => {
-  x.src = x.src.replace(/large/,'medium')
-  console.log(x);
 });
