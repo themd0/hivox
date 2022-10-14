6bb1184b78b77b0c0e5e3d9253e015f07931a89f
themd0 <90915458+themd0@users.noreply.github.com>
2022-10-14 03:34:34 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -21,11 +21,11 @@ $('.nsv-2').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?target=video'));
 });
 
-$('.nsv-4').each(function() {
+$('.nsv-4, .d-ak-360,.d-ak-720').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?package=com.dv.adm'));
 });
 
-$('.nsv-5, .ak-but-h, .ak-but-s').each(function() {
+$('.nsv-5, .ak-but-h, .ak-but-s, .w-ak-360,.w-ak-720').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?package=com.anifort.player'));
 });
 
