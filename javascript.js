53df8f0c3380c41e0ce0d3a7f8ab8de5d311b1e8
themd0 <90915458+themd0@users.noreply.github.com>
2022-10-15 02:28:37 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,18 +1,12 @@
-function myFunction() {
-var input, filter, ul, li, a, i, txtValue;
-input = document.getElementById("myInput");
-filter = input.value.toUpperCase();
-ul = document.getElementById("myUL");
-li = ul.getElementsByTagName("li");
-for (i = 0; i < li.length; i++) {
-a = li[i].getElementsByTagName("a")[0];
-txtValue = a.textContent || a.innerText;
-if (txtValue.toUpperCase().indexOf(filter) > -1) {
-li[i].style.display = "";
-} else {
-li[i].style.display = "none";
-}
-}
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos &gt; currentScrollPos) {
+    document.getElementById(&quot;xx-input&quot;).style.top= &quot;20px&quot;;
+  } else {
+    document.getElementById(&quot;xx-input&quot;).style.top= &quot;-50px&quot;;
+  }
+  prevScrollpos = currentScrollPos;
 }
 
 $('.nsv-2').each(function() {
