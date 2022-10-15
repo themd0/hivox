2440633455208913a9836f827f85d3459e9ca782
themd0 <90915458+themd0@users.noreply.github.com>
2022-10-15 02:29:57 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,19 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos &gt; currentScrollPos) {
-    document.getElementById(&quot;xx-input&quot;).style.top= &quot;20px&quot;;
-  } else {
-    document.getElementById(&quot;xx-input&quot;).style.top= &quot;-50px&quot;;
-  }
-  prevScrollpos = currentScrollPos;
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("xx-input");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("xx-ul");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
-
 $('.nsv-2').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?target=video'));
 });
