f027d47fcbd76a495183dc5c1a8e5b4038068486
themd0 <90915458+themd0@users.noreply.github.com>
2022-10-16 15:24:50 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -17,14 +17,14 @@ function toggleDiv(divid)
     }
 } 
 
-$('.nsv-2').each(function() {
+$('.nsv-3, .ak-but-h, .ak-but-s, .w-ak-360, .w-ak-720').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?target=video'));
 });
 
-$('.nsv-4, .d-ak-360, .d-ak-720').each(function() {
+$('.nsv-2, .nsv-4, .d-ak-360, .d-ak-720').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?package=com.dv.adm'));
 });
 
-$('.nsv-5, .ak-but-h, .ak-but-s, .w-ak-360, .w-ak-720').each(function() {
+$('.nsv-5').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?package=com.anifort.player'));
 });
