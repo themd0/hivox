68a30bc3289830effad85ab649140846b0c3b8a8
themd0 <90915458+themd0@users.noreply.github.com>
2022-10-29 20:11:46 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -29,7 +29,7 @@ $('.nsv-5').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?target=video'));
 });
 
-var divs = ["Menu1", "Menu2", "Menu3", "Menu4", "Menu5", "Menu6", "Menu7"];
+var divs = ["nseason1", "nseason2", "nseason3", "nseason4", "nseason5", "nseason6", "nseason7"];
 var visibleDivId = null;
 function toggleVisibility(divId) {
   if(visibleDivId === divId) {
