8ef1f05d46b3ad0e3ae07f7a9462ab48707775b5
themd0 <90915458+themd0@users.noreply.github.com>
2022-10-29 19:49:04 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,27 +1,3 @@
-var divs = ["Menu1", "Menu2", "Menu3", "Menu4", "Menu5", "Menu6", "Menu7"];
-var visibleDivId = null;
-function toggleVisibility(divId) {
-  if(visibleDivId === divId) {
-    //visibleDivId = null;
-  } else {
-    visibleDivId = divId;
-  }
-  hideNonVisibleDivs();
-}
-function hideNonVisibleDivs() {
-  var i, divId, div;
-  for(i = 0; i < divs.length; i++) {
-    divId = divs[i];
-    div = document.getElementById(divId);
-    if(visibleDivId === divId) {
-      div.style.display = "block";
-    } else {
-      div.style.display = "none";
-    }
-  }
-}
-
-
 function toggleDiv(divid)
   {
  
@@ -52,3 +28,26 @@ $('.nsv-2, .nsv-4, .d-ak-360, .d-ak-720').each(function() {
 $('.nsv-5').each(function() {
   $(this).attr('href', $(this).attr('href').replace('.mp4','.mp4?/?target=video'));
 });
+
+var divs = ["Menu1", "Menu2", "Menu3", "Menu4", "Menu5", "Menu6", "Menu7"];
+var visibleDivId = null;
+function toggleVisibility(divId) {
+  if(visibleDivId === divId) {
+    //visibleDivId = null;
+  } else {
+    visibleDivId = divId;
+  }
+  hideNonVisibleDivs();
+}
+function hideNonVisibleDivs() {
+  var i, divId, div;
+  for(i = 0; i < divs.length; i++) {
+    divId = divs[i];
+    div = document.getElementById(divId);
+    if(visibleDivId === divId) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+}
