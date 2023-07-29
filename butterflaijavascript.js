dd00f1124fa5b98bf45881e4e7f30749f78d1024
themd0 <90915458+themd0@users.noreply.github.com>
2023-07-29 19:26:05 +0100

    Update butterflaijavascript.js

--- butterflaijavascript.js
+++ butterflaijavascript.js
@@ -1,3 +1,3 @@
-$('.extL alt').each(function() {
-  $(this).attr('href', $(this).attr('href').replace('/search/label/','https://klaster-news-13.blogspot.com/search/label/'));
+$('.extL.alt').each(function() {
+  $(this).attr('href', $(this).attr('href').replace('/search', 'https://klaster-news-13.blogspot.com/search'));
 });
